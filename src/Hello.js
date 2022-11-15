4a4859a0242b31c3ded07cd181a200abbe3f2005
amapic <amaury.pichat@gmail.com>
2022-11-15 10:34:55 +0100

    Initial commit

---
+++ src/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Rrrr() {
+  return <div>EE</div>;
+}
+
+export default function Hello() {
+  return (
+    //<>
+    <h1>Hello !</h1>
+    //<Rrrr />
+    // </>
+  );
+}
---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+it("works", () => {
+  expect(1 + 1).toBe(2);
+});
+
+it("doesn't work", () => {
+  expect(1 + 1).toBe(2);
+});
+
+//
